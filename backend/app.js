cbeabd9529e7ea9a61b6f776e5cc39a1462cc4d7
likhnic <likhith26090@gmail.com>
2022-12-06 18:41:36 +0530

    changed package.json

--- backend/app.js
+++ backend/app.js
@@ -127,6 +127,6 @@ app.post('/newdetails', fetchuser, async (req, res) => {
     res.json({ currdetails })
 })
 
-app.listen(5000, () => {
-    console.log("Listening to port 5000");
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Listening to port ${process.env.PORT}`);
 })
