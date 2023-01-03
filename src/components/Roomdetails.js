af6218c2129d1d8559cb4c2e46af20af27b97db1
likhnic <likhith26090@gmail.com>
2023-01-03 19:47:34 +0530

    Responsive

--- src/components/Roomdetails.js
+++ src/components/Roomdetails.js
@@ -8,34 +8,34 @@ const Roomdetails = (props) => {
             {details.name && (<div className="card m-2">
                 <div className="card-body">
                     <div className="row">
-                        <div className="col-md-6">
+                    <div className="col-4 col-lg-5">
                             Name:
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-8 col-lg-5">
                             {details.name}
                         </div>
-                        <div className="col-md-6">
-                            Roll Number:
+                        <div className="col-4 col-lg-5">
+                            Roll No.:
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-8 col-lg-5">
                             {details.rollno}
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-4 col-lg-5">
                             Email:
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-8 col-lg-5">
                             {details.email}
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-4 col-lg-5">
                             Contact:
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-8 col-lg-5">
                             {details.contact}
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-4 col-lg-5">
                             Room:
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-8 col-lg-5">
                             {(details.roomid || details.roomid === 0) ? details.roomid : "Room is not yet assigned"}
                         </div>
                     </div>
